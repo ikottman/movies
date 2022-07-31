18d3e6ef7bb46e90b469a80d0708e447ad1bfebd
ikottman <ikottman@users.noreply.github.com>
2022-07-31 12:02:22 -0500

    font and set emoji at cursor

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,48 @@
 <script lang="ts">
   import EmojiSelector from 'svelte-emoji-selector';
 
-  let movie = '';
-  function onEmoji(event) {
-    movie += event.detail;
+  let movies = "ian test";
+  
+  function placeEmojiAtCursor(event) {
+    const textArea = document.getElementById('movies');
+    const emoji = event.detail;
+    if (textArea.selectionStart || textArea.selectionStart == '0') {
+        var startPos = textArea.selectionStart;
+        var endPos = textArea.selectionEnd;
+        textArea.value = textArea.value.substring(0, startPos)
+            + emoji
+            + textArea.value.substring(endPos, textArea.value.length);
+    } else {
+      textArea.value += emoji;
+    }
   }
 
-  let movies = [];
-
   function save() {
-    movies = [...movies, movie];
-    movie = '';
+    const updatedMovies = document.getElementById('movies').value;
+    console.log(updatedMovies);
   }
 </script>
 
 <main>
-  <ul>
-    {#each movies as movie}
-      <li>{movie}</li>
-    {/each}
-  </ul>
-  <input bind:value={movie} type="text"/>
-  <EmojiSelector on:emoji={onEmoji} />
+  <textarea id='movies' bind:value={movies} rows={movies.split('\n').length + 5}, cols=70/>
+  <br/>
+  <EmojiSelector on:emoji={placeEmojiAtCursor} />
+  <br/>
   <button on:click={save}>💾</button>
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Nunito Sans', sans-serif;
   }
 
   main {
     text-align: center;
     padding: 1em;
     margin: 0 auto;
+  }
+
+  textarea {
+    font-size: large;
   }
 </style>
